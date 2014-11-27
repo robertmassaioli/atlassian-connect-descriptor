7c817027dceb362534805f2661a4258e5ad57552
Robert Massaioli <rmassaioli@atlassian.com>
2014-11-28 08:51:36 +1100

    We have our first working test case.

--- tests/ConditionsTest.hs
+++ tests/ConditionsTest.hs
@@ -1,9 +1,22 @@
-module ConditionsTest where
+module ConditionsTest (conditionsTests) where
 
-import qualified Distribution.TestSuite as TS
-import qualified Test.HUnit as HU
+import Data.Connect.Conditions
+import Test.HUnit
+
+conditionsTests :: Test
+conditionsTests = TestList
+    [ invertConditionSimple
+    ]
 
 -- Inverting conditions works correctly
+invertConditionSimple :: Test
+invertConditionSimple = TestCase $ do
+    let originalCondition = staticJiraCondition UserIsAdminJiraCondition
+    (not . conditionInverted $ originalCondition) @? "Expected the condition to not be inverted on creation."
+    let invertedCondition = invertCondition originalCondition
+    conditionInverted invertedCondition @? "Expected the condition to be inverted."
+
+
 -- JIRA Conditions come out correctly
 -- Confluence Conditions come out correctly
 -- Remote conditions come out correctly
--- tests/Test.hs
+++ tests/Test.hs
@@ -1,14 +1,14 @@
 module Test where
 
+import           ConditionsTest
 import qualified Distribution.TestSuite as TS
 import qualified Test.HUnit             as HU
 import           Util
 
-test1 :: HU.Test
-test1 = HU.TestCase (HU.assertEqual "one equals three" 1 3)
-
 hunitTests :: HU.Test
-hunitTests = HU.TestList [HU.TestLabel "Test 1" test1]
+hunitTests = HU.TestList
+    [ HU.TestLabel "Condition Tests" conditionsTests
+    ]
 
 tests :: IO [TS.Test]
 tests = return [ TS.Test (hunitTestInstance hunitTests) ]
