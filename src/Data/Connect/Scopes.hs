fb729100db0ed2e9a162c19b4a5ec0f2b31d9df2
Robert Massaioli <rmassaioli@atlassian.com>
2017-05-13 13:21:11 +1000

    MR-66: Added in the ACT_AS_USER scope.

--- src/Data/Connect/Scopes.hs
+++ src/Data/Connect/Scopes.hs
@@ -23,6 +23,7 @@ data ProductScope
   | SpaceAdmin -- ^ A Confluence specific scope. Lets your add-on administer a space in Confluence.
   | Admin -- ^ Gives your Atlassian Connect add-on administrative rights to the Cloud instance. (But NOT system
           -- administrator permission. Happily you cannot request that level of access.)
+  | ActAsUser -- ^ Add-ons with this scope can access resources and perform actions in JIRA and Confluence on behalf of users.
   deriving (Show, Eq, Generic)
 
 instance ToJSON ProductScope where
@@ -32,6 +33,7 @@ instance ToJSON ProductScope where
    toJSON ProjectAdmin = "project_admin"
    toJSON SpaceAdmin   = "space_admin"
    toJSON Admin        = "admin"
+   toJSON ActAsUser    = "act_as_user"
 
 instance FromJSON ProductScope where
   parseJSON (String "read")             = return Read
@@ -40,4 +42,5 @@ instance FromJSON ProductScope where
   parseJSON (String "project_admin")    = return ProjectAdmin
   parseJSON (String "space_admin")      = return SpaceAdmin
   parseJSON (String "admin")            = return Admin
+  parseJSON (String "act_as_user")      = return ActAsUser
   parseJSON _                           = mzero
